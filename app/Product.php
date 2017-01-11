ca696c311ad7b72a13a8a765515f4f9f501c322a
Temitope Olotin <temitope.olotin@andela.com>
2017-01-11 12:42:15 +0100

    Define relationship with category as many to one

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
 	protected $fillable = ['name', 'price', 'quantity', 'category_id'];
+
+	public function category()
+	{
+		return $this->belongsTo('Laztopaz\Model\Category');
+	}
 }
