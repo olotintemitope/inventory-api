e2eac83ad7f8aa2b461bbbcb6092f16af70c21b1
Temitope Olotin <temitope.olotin@andela.com>
2017-01-11 16:08:20 +0100

    resolve merge conflict

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,11 +28,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-<<<<<<< HEAD
             //\Laztopaz\Http\Middleware\VerifyCsrfToken::class,
-=======
-            ///\Laztopaz\Http\Middleware\VerifyCsrfToken::class,
->>>>>>> ft-create-tests
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
